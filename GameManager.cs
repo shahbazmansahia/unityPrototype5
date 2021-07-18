2bd814806bde73b4fcacd3531ff27fe21d360096
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-07-18 04:31:17 -0700

    Added the difficulty buttons and settings. Also added my own custom title and game over screens with start and exit buttons; these are bound to come in handy later on in the project.

--- GameManager.cs
+++ GameManager.cs
@@ -12,19 +12,21 @@ public class GameManager : MonoBehaviour
 
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI titleText;
     public Button restartButton;
+    public Button startButton;
+    public Button exitButton;
+    public Button[] difficultyButtons;
+    public Button BackButt;
+    public GameObject titleScreen;
+
     private int score;
 
     public bool isGameActive;
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        gameOverText.gameObject.SetActive(false);
-        restartButton.gameObject.SetActive(false);
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+        //StartGame();
         
     }
 
@@ -55,16 +57,79 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    /** Spawns the game over screen elements and gives us the option to restart the game
+     * 
+     */
     public void GameOver()
     {
         isGameActive = false;
         gameOverText.gameObject.SetActive(true);
         restartButton.gameObject.SetActive(true);
+        exitButton.gameObject.SetActive(true);
     }
 
+    /** This method resets and reloads the scene from the beginning; FIX ME: This is for some reason turning off the lights in the scene? or dimming the brightness?
+     * 
+     */
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    /** This function starts the game; activates after the choose difficulty screen
+     * param: difficulty - a difficulty tweaker that changes the spawnRate of objects in the game; we get this value from the difficulty buttons
+     */
+    
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        gameOverText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        spawnRate /= difficulty;
+        titleScreen.gameObject.SetActive(false);
+        
+    }
+
+    /** Exits the game; Meant to be used at the title and game over screens
+     * 
+     */
+    public void ExitGame()
+    {
+        //ExitGame();
+    }
+
+    /** This method takes us to the choose difficulty screen
+     * 
+     */
+    public void ChooseDifficulty()
+    {
+        startButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(false);
+
+        for (int i = 0; i < difficultyButtons.Length; i++)
+        {
+            difficultyButtons[i].gameObject.SetActive(true);
+        }
+
+        BackButt.gameObject.SetActive(true);
+    }
+
+    /** This method makes us go back to the title screen at the start.
+     * 
+     */
+    public void Back2Title()
+    {
+        startButton.gameObject.SetActive(true);
+        exitButton.gameObject.SetActive(true);
+
+        for (int i = 0; i < difficultyButtons.Length; i++)
+        {
+            difficultyButtons[i].gameObject.SetActive(false);
+        }
+
+        BackButt.gameObject.SetActive(false);
+    }
 }
